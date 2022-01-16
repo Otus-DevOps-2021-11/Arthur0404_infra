a4c393add87483a2743a7e95d4d4d201c21038b1
abadmaev <https://github.com/Natasha2202/Matchtv_web>
2022-01-16 17:32:53 +0300

    Update_Terraform2_HomeWork

--- terraform/prod/variables.tf
+++ terraform/prod/variables.tf
@@ -34,3 +34,13 @@ variable private_key {
 variable instances_count {
   default = 1
 }
+
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default     = "reddit-app-base"
+}
+
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default     = "reddit-db-base"
+}
